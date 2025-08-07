d25f76288779e25ee643ca18a9b0e1edac196eb0
Moslm Shlaka <mike7olwo@gmail.com>
2025-08-07 22:35:59 +0300

    turned Answer options to buttons

--- app/src/main/java/com/example/falshcardsquiz/components/AnswerOptions.kt
+++ app/src/main/java/com/example/falshcardsquiz/components/AnswerOptions.kt
@@ -1,16 +1,25 @@
 package com.example.falshcardsquiz.components
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun AnswerOptions(options:List<String>) {
     options.forEach { opt ->
-        Text(text = opt)
+
+        Button({},    colors = ButtonDefaults.buttonColors(
+            containerColor = Color(0xFF3F5C95), // Background color
+            contentColor = Color.White, // Text/icon color
+        ), modifier = Modifier.width(160.dp)) { Text(text = opt) }
         Spacer(modifier = Modifier.height(8.dp))
     }
 }
